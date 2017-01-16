afe1858ea0b5d56e69ec1d3d174ce8cd8a2399db
JustToBeBetter <likeme@li.local>
2017-01-16 13:40:38 +0800

    Initial Commit

---
+++ LJZArchiveDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LJZArchiveDemo
+//
+//  Created by DBOX on 2017/1/16.
+//  Copyright © 2017年 DBOX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LJZArchiveDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LJZArchiveDemo
+//
+//  Created by DBOX on 2017/1/16.
+//  Copyright © 2017年 DBOX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

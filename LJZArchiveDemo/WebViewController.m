7e9aed8f255e8905f5ae76e9ab00ee1ae1a47676
lijinzhu <lijinzhu@dobest.com>
2020-03-05 10:15:09 +0800

    打开文档

--- LJZArchiveDemo/WebViewController.m
+++ LJZArchiveDemo/WebViewController.m
@@ -9,7 +9,7 @@
 #import "WebViewController.h"
 #import <WebKit/WebKit.h>
 
-@interface WebViewController ()
+@interface WebViewController ()<UIWebViewDelegate,WKNavigationDelegate>
 {
     WKWebView *_wkWebView;
     UIWebView *_webView;
@@ -21,17 +21,60 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    _wkWebView = [[WKWebView alloc]initWithFrame:CGRectMake(0, 64,kScreenWidth , kScreenHeight-64)];
-    
+    _wkWebView = [[WKWebView alloc]initWithFrame:self.view.frame];
+    _wkWebView.navigationDelegate = self;
     NSURL *url = [NSURL fileURLWithPath:self.path];
-    [_wkWebView loadRequest:[NSURLRequest requestWithURL:url]];
+//    [_wkWebView loadRequest:[NSURLRequest requestWithURL:url]];//测试iOS 13读取本地文件
+    [_wkWebView loadFileURL:url allowingReadAccessToURL:url];//读取本地文件 iOS 9 之后
+    
     [self.view addSubview:_wkWebView];
-    _webView = [[UIWebView alloc]initWithFrame:self.view.frame];
+//    _webView = [[UIWebView alloc]initWithFrame:self.view.frame];
+//    _webView.delegate = self;
+//    [_webView loadRequest:[NSURLRequest requestWithURL:url]];
+//    [self.view addSubview:_webView];
+//    [self getMimeType:self.path];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (void)getMimeType:(NSString *)path{
+    // 创建URL
+    NSURL *url = [NSURL fileURLWithPath:path];
+    // 创建请求对象
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    // 发送异步请求 在请求的
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse * _Nullable response, NSData * _Nullable data, NSError * _Nullable connectionError) {
+        NSLog(@"%@",response.MIMEType);
+    }];
+}
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    NSString *pageUrl = request.URL.absoluteString;
+    NSString *lastName =[[pageUrl lastPathComponent] lowercaseString];
+    
+    if ([lastName containsString:@".pdf"])
+    {
+        NSData *data = [NSData dataWithContentsOfURL:request.URL];
+        [_webView loadData:data MIMEType:@"application/pdf" textEncodingName:@"GBK" baseURL:nil];
+    }
+    return YES;
+
+}
+
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
+{
+    NSString *_webUrlStr = navigationAction.request.URL.absoluteString;
+    NSString *lastName =[[_webUrlStr lastPathComponent] lowercaseString];
+    
+    if ([lastName containsString:@".pdf"])
+    {
+        NSData *data = [NSData dataWithContentsOfURL:navigationAction.request.URL];
+        [_wkWebView loadData:data MIMEType:@"application/pdf" characterEncodingName:@"GBK" baseURL:nil];
+    }
+    decisionHandler(WKNavigationActionPolicyAllow);
 }
 
 /*
